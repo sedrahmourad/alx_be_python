75993d3634705955c8a414abdff6c033fa545d6f
sedrah mourad <sedramourad@icloud.com>
2025-08-01 13:28:28 +0300

    add basic operations inside python introduction

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+add = number1 + number2
+sub = number1 - number2
+prod = number1 * number2
+print("addition of",number1,"and",number2,"is",add)
+print("subtraction of",number1,"and",number2,"is",sub)
+print("multplication of",number1,"and",number2,"is",prod)
