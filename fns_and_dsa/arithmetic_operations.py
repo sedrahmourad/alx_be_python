bd65d828c28a5d30175eb51ac1f6bf977b888063
sedrah mourad <sedramourad@icloud.com>
2025-08-14 01:27:53 +0300

    add arithmetic operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,13 @@
+def perform_operation(num1, num2, operation):
+    if operation == "add":
+        result = num1 + num2
+    elif operation == "subtract":
+        result = num1 - num2
+    elif operation == "multiply":
+        result = num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            return "error: division by zero"
+        result = num1 / num2
+    else:
+        print("wrong operation")
