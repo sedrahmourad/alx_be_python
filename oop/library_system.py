94c1374a3b7270ef3393d7d67345322edb3b6562
sedrah mourad <sedramourad@icloud.com>
2025-09-07 17:41:28 +0300

    add 2

--- oop/library_system.py
+++ oop/library_system.py
@@ -9,7 +9,7 @@ class EBook(Book):
         super().__init__(title, author)
         self.file_size = file_size
     def __str__(self):
-        return f"Ebook: {self.title} by {self.author}, File Size: {self.file_size}KB)"
+        return f"Ebook: {self.title} by {self.author}, File Size: {self.file_size}KB"
     
 
 class PrintBook(Book):
@@ -17,7 +17,7 @@ class PrintBook(Book):
         super().__init__(title, author)
         self.page_count = page_count
     def __str__(self):
-        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count})"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 class Library:
     def __init__(self):
         self.books = []
