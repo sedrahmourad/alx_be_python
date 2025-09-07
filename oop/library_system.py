c769ab2d51cfcd18449b25c24c6ffc47d7dc85c6
sedrah mourad <sedramourad@icloud.com>
2025-09-07 17:34:07 +0300

    add library systems 3

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,17 +1,23 @@
 class Book:
     def __init__(self,title: str,author: str):
         self.title = title
-        self.author = author 
+        self.author = author
+    def __str__(self):
+        return f"{self.title} by {self.author}"
 class EBook(Book):
     def __init__(self, title: str, author: str, file_size: int):
         super().__init__(title, author)
         self.file_size = file_size
+    def __str__(self):
+        return f"{self.title} by {self.author} (EBook, {self.file_size}MB)"
+    
 
 class PrintBook(Book):
     def __init__(self, title: str, author: str, page_count: int):
         super().__init__(title, author)
         self.page_count = page_count
-    
+    def __str__(self):
+        return f"{self.title} by {self.author} (Print, {self.page_count} pages)"
 class Library:
     def __init__(self):
         self.books = []
